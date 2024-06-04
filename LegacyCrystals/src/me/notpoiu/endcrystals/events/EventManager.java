88ef8e68dad04815943694e9a4178aa139594b6e
firstlynx-sys <140927451+firstlynx-sys@users.noreply.github.com>
2024-06-04 13:47:35 -0400

    Fix taking crystals rom inventory when clicking on a block that already has a crystal on it and crystals still placing when there is a block on top of it

--- LegacyCrystals/src/me/notpoiu/endcrystals/events/EventManager.java
+++ LegacyCrystals/src/me/notpoiu/endcrystals/events/EventManager.java
@@ -13,61 +13,46 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import me.notpoiu.endcrystals.item.ItemManager;
 
 public class EventManager implements Listener {
-	
-	@EventHandler
-    public static void onRightClick(PlayerInteractEvent event) {
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getItem() != null && event.getItem().getItemMeta().equals(ItemManager.instance.crystalItem.getItemMeta()) && (event.getClickedBlock().getType() == Material.OBSIDIAN || event.getClickedBlock().getType() == Material.BEDROCK)) {
+
+    @EventHandler
+    public void onRightClick(PlayerInteractEvent event) {
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getItem() != null && event.getItem().isSimilar(ItemManager.instance.crystalItem) && (event.getClickedBlock().getType() == Material.OBSIDIAN || event.getClickedBlock().getType() == Material.BEDROCK)) {
             
-        	Location loc = new Location(event.getPlayer().getWorld(), (double)event.getClickedBlock().getX() + 0.5, event.getClickedBlock().getY() + 1, (double)event.getClickedBlock().getZ() + 0.5);
-        	
-        	if(!event.getPlayer().getGameMode().equals(GameMode.CREATIVE)) {
-        		
-        		if(event.getItem().getAmount() == 1) {
-        			event.getPlayer().getInventory().remove(event.getItem());
-        			event.getPlayer().updateInventory();
-        		}else {
-        			event.getItem().setAmount(event.getItem().getAmount() - 1);
-    	            
-    	            event.getPlayer().updateInventory();
-        		}
-        		
-	            
+            Location loc = new Location(event.getPlayer().getWorld(), event.getClickedBlock().getX() + 0.5, event.getClickedBlock().getY() + 1, event.getClickedBlock().getZ() + 0.5);
+            Location aboveLoc = new Location(event.getPlayer().getWorld(), loc.getX(), loc.getY() + 1, loc.getZ());
+
+            if (aboveLoc.getBlock().getType() != Material.AIR) {
+                return;
             }
-        	
-        	if(getNearbyEntitiesAndIsEndCrystal(loc, 1.5)) {
-        		return;
-        	}else {
-        		event.getPlayer().getWorld().spawnEntity(loc, EntityType.ENDER_CRYSTAL);
-        	}
+
+            if (getNearbyEntitiesAndIsEndCrystal(loc, 0.5)) {
+                return;
+            }
             
+            if (!event.getPlayer().getGameMode().equals(GameMode.CREATIVE)) {
+                if (event.getItem().getAmount() == 1) {
+                    event.getPlayer().getInventory().remove(event.getItem());
+                } else {
+                    event.getItem().setAmount(event.getItem().getAmount() - 1);
+                }
+                event.getPlayer().updateInventory();
+            }
+            
+            event.getPlayer().getWorld().spawnEntity(loc, EntityType.ENDER_CRYSTAL);
         }
     }
     
-    public static boolean getNearbyEntitiesAndIsEndCrystal(Location where, double range) {
-    	
-    	for (Entity entity : where.getWorld().getEntities()) {
-    		if(amogus)
-    			break;
-    		
-    		if (isInBorder(where, entity.getLocation(), range)) {
-    			if(entity.getType() == EntityType.ENDER_CRYSTAL) {
-    				return true;
-    			}
-    		}
-    	}
-    	
-    	return false;
+    public boolean getNearbyEntitiesAndIsEndCrystal(Location where, double range) {
+        for (Entity entity : where.getWorld().getEntities()) {
+            if (isInBorder(where, entity.getLocation(), range) && entity.getType() == EntityType.ENDER_CRYSTAL) {
+                return true;
+            }
+        }
+        return false;
     }
     
-    public static boolean isInBorder(Location center, Location notCenter, double range) {
-    	int x = center.getBlockX(), z = center.getBlockZ();
-    	int x1 = notCenter.getBlockX(), z1 = notCenter.getBlockZ();
-    	 
-    	if (x1 >= (x + range) || z1 >= (z + range) || x1 <= (x - range) || z1 <= (z - range)) {
-    		return false;
-    	}
-    		return true;
+    public boolean isInBorder(Location center, Location notCenter, double range) {
+        double distance = center.distance(notCenter);
+        return distance <= range;
     }
-    
-    
 }
